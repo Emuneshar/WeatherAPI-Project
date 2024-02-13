deefe5a734664172783d7f9ce4177ca4d2f99a95
Eshwar Muneshar <Emuneshar@gmail.com>
2024-02-12 19:31:33 -0500

    cleaning up code

--- weather app.py
+++ weather app.py
@@ -2,7 +2,7 @@ import requests
 
 apiKeys = "54fa0149f32598b8de4cf172383a469b"
 
-city = input("Please enter the name of a city")
+city = input("Please enter the name of a city: \n")
 
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKeys}'
 
