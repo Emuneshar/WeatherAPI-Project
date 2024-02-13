2a4a183ed71f52cfa881d72b83a7c6f2f565039f
Eshwar Muneshar <Emuneshar@gmail.com>
2024-02-12 19:22:30 -0500

    used get function to get json response from website

--- weather app.py
+++ weather app.py
@@ -5,3 +5,5 @@ apiKeys = "54fa0149f32598b8de4cf172383a469b"
 city = input("Please enter the name of a city")
 
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKeys}'
+
+response = requests.get(url)
